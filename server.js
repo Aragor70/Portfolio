e052363a86dfb8593db7a33fa9b736053560f14d
Aragor70 <mikis3@o2.pl>
2021-01-24 21:40:48 +0000

    mobile front page

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => console.log(`Server started in ${process.env.NODE_ENV} mode on port ${PORT}.`.green));
+const server = app.listen(PORT, () => console.log(`Server started in ${process.env.NODE_ENV} mode on port ${PORT}.`));
 
 process.on('unhandledRejection', (err, _promise) => {
-    console.log(`Error message: ${err.message}`.red)
+    console.log(`Error message: ${err.message}`)
     server.close(() => process.exit(1))
 })
