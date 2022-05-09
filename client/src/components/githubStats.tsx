69cbc52e3e9fc981b7cbf4c786a86d4618e8a0d6
Aragor70 <mikis3@o2.pl>
2022-05-09 21:05:08 +0000

    Client update: Include github-api responsive design

--- client/src/components/githubStats.tsx
+++ client/src/components/githubStats.tsx
@@ -2,20 +2,26 @@ import moment from 'moment';
 import React, { Fragment } from 'react';
 
 import { ReactComponent as CommitSvg} from '../style/icons/code-commit-solid.svg'
+import { ReactComponent as UpdateSvg} from '../style/icons/refresh-outline.svg'
+import { ReactComponent as CreateSvg} from '../style/icons/create-outline.svg'
 
 const GithubStats = ({ repos, name }: any) => {
 
     const repository : any = (repos.filter((element: any) => element?.name === name))[0] || null
     
     if (!repository) return null;
-
+    console.log(repository?.created_at)
     return (
         <Fragment>
             <p className="commits">
-                <CommitSvg /> <span>{repository?.commits?.length || 0} commits</span>
-            </p>
-            <p>
-                <i className="fas fa-calendar-plus"></i> ver.{ moment(repository?.commits[0]?.commit?.author?.date).format('DD-MM-YYYY') || 'None'}
+                <span>
+                    <CommitSvg /> <span>{repository?.commits?.length || 0} commits</span>
+                </span>
+            
+                <span>
+                    <CreateSvg /> { moment(repository?.created_at)?.format('DD-MM-YYYY') || ''}
+                </span>
+                {!!repository?.commits?.length && <span><UpdateSvg /> {moment(repository?.commits[0]?.commit?.author?.date).format('DD-MM-YYYY')}</span>}
             </p>
         </Fragment>
     );
