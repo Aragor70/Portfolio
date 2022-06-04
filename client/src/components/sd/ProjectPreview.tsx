60d527ced6ad7fff515c17b33bc32d82a850b270
Aragor70 <mikis3@o2.pl>
2022-06-04 17:10:21 +0000

    Client update: Improve project preview

--- client/src/components/sd/ProjectPreview.tsx
+++ client/src/components/sd/ProjectPreview.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import HtmlParser from 'react-html-parser';
+import { Link } from 'react-router-dom';
 /* 
 import { ReactComponent as CommitSvg} from '../style/icons/code-commit-solid.svg'
 import { ReactComponent as UpdateSvg} from '../style/icons/refresh-outline.svg'
@@ -11,7 +12,7 @@ const ProjectPreview = ( props: any ) => {
         <Fragment>
             <div className="section-image">
                 {
-                    props?.images && <img src={props?.images[0]} alt="projects_image" />
+                    !!props.images?.length && <img src={props?.images[0]?.name} alt={props.images[0]?.label} onError={(e: any) => {e.target.style.display = 'none'}} />
                 }
                 
             </div>
@@ -19,17 +20,24 @@ const ProjectPreview = ( props: any ) => {
                 <h3 className="content-center"><span style={{ fontSize: '45px' }}>{props?.title && HtmlParser(props?.title)}</span></h3>
                 
                 { props?.text && HtmlParser(props?.text) }
-
+                
                 <ul className="icons">
                     {
-                        props?.icons && props?.icons?.map((element: any) => <li className="icon"><img src={element} alt="typeScript" /><span>TypeScript</span></li>)
+                        !!props.icons?.length && props?.icons?.map((element: any) => element.isFile ? <li className="icon"><img src={`/assets/icons/${element.name}`} alt={element.label} /><span>{element.label}</span></li> : <li className="icon"><i className={element.name}></i><span>{element.label}</span></li>)
                     }
                 </ul>
                 
                 <ul className="more-about" >
-                    {/* <span onClick={() => history.push(`${url}/NiVest-Software`)}>View project details</span> */}
-                    <li className="icon-box" onClick={() => window.open("https://redirect.m-prus.uk/NiVest_Software-GitHub_Repository", "_blank")}><i className="fas fa-code fa"></i></li>
-                    <li className="icon-box" onClick={() => window.open("https://nivest-software.m-prus.uk/", "_blank")}><i className="fab fa-chrome fa"></i></li>
+                    {
+                        !!props.name && <Link to={`/software_projects/${props.name}`}>View project details</Link>
+                    }
+                    {
+                        !!props.repositories?.length && <li className="icon-box" onClick={() => window.open(props?.repositories[0]?.url, "_blank")}><i className="fas fa-code fa"></i></li>
+                    }
+                    {
+                        !!props.website && <li className="icon-box" onClick={() => window.open(props.website, "_blank")}><i className="fab fa-chrome fa"></i></li>
+                    }
+                    
                 </ul>
             
             </div>
