9c05bd2082629069e19d7bc1c44531cb8e1ab38e
Aragor70 <mikis3@o2.pl>
2022-07-29 17:39:14 +0000

    Client update: Include production webpack

--- client-update/webpack.prod.config.ts
+++ client-update/webpack.prod.config.ts
@@ -1,9 +1,18 @@
 import path from "path";
-import { Configuration } from "webpack";
+import { Configuration as WebpackConfiguration, HotModuleReplacementPlugin } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+
+
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import ESLintPlugin from "eslint-webpack-plugin";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
 
 const config: Configuration = {
   mode: "production",
@@ -15,61 +24,71 @@ const config: Configuration = {
   },
   module: {
     rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
+          {
+            test: /\.(ts|js)x?$/i,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  "@babel/preset-env",
+                  "@babel/preset-react",
+                  "@babel/preset-typescript",
+                ],
+              },
+            },
+          },
+          {
+            test: /\.s?css$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'style-loader',
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                },
             ],
           },
-        },
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.s?css$/,
-        exclude: /node_modules/,
-        use: [
-            {
-                loader: 'style-loader',
+          {
+            test: /\.(png|jpe?g|ico|gif)$/,
+            use: {
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]',
+              },
             },
-            {
-                loader: 'css-loader',
-                options: {
-                    sourceMap: true,
-                },
-            },
-            {
-                loader: 'sass-loader',
-                options: {
-                    sourceMap: true,
-                },
-            }
-        ],
-      },
-    ],
+          },
+          {
+            test: /\.svg$/,
+            use: ['@svgr/webpack', 'url-loader'],
+          }
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "public/index.html",
     }),
+    new HotModuleReplacementPlugin(),
     new ForkTsCheckerWebpackPlugin({
-      async: false,
+        async: false
     }),
     new ESLintPlugin({
-      extensions: ["js", "jsx", "ts", "tsx", "png"],
+        extensions: ["js", "jsx", "ts", "tsx"],
     }),
-    new CleanWebpackPlugin(),
   ],
 };
 
